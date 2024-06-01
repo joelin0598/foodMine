4b7b33c997988121606325a384fc13a0f82e198b
joelin0598 <jguamuchm@miumg.edu.gt>
2024-05-31 22:52:01 -0600

    Configuración db

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -2,9 +2,9 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
     user: 'admin',
-    host: 'dpg-cp8vgf5ds78s73cacr10-a.oregon-postgres.render.com',
-    database: 'ejercicio1',
-    password: 'fmCgLTDAWmeV5zOMfbNG9Pl2WLQZbWcc',
+    host: 'dpg-cpd461674orc73a08mv0-a.oregon-postgres.render.com',
+    database: 'exercisefoodmain',
+    password: '91ZgadcLhbTUQz5pSqHFNY5ikDDV46dq',
     port: parseInt('5432'),
     ssl: {
         rejectUnauthorized: false  // Importante en entornos de desarrollo con SSL autofirmado, ajustar para producción
